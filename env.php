7ad503ccf1491e9d6d3e436381f8c8e230e03315
joansor <sor.joan@gmail.com>
2020-01-31 15:42:44 +0100

    first commit

---
+++ env.php
@@ -0,0 +1,18 @@
+<?php
+
+//Nommer ces variables correctement, ce fichier pourra être dans un .gitignore
+$host = "localhost";
+$dbname = "mailproject; port=3808; charset=utf8";
+$user = "root";
+$pass = "";
+$projetName = "mailproject";
+/*
+$host = "localhost";
+$dbname = "joans_; port=3808; charset=utf8";
+$user = "joans";
+$pass = "knjs9opz0AHmqA==";
+$projetName = "joans_";*/
+
+$projectNameLength = strlen($projetName) + 1;
+$base_url = mb_substr($_SERVER['REQUEST_URI'], 0, $projectNameLength);
+
